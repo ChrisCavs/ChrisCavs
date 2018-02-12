f2ff99f881883dccff7f0a3f28bd82c15b967c28
Chris <chris211321@gmail.com>
2018-02-12 11:57:04 -0500

    smooth scroll working

--- src/js/smoothScroll.js
+++ src/js/smoothScroll.js
@@ -4,26 +4,23 @@ const smoothScroll = (destination) => {
 
   const destScroll = destElement.getBoundingClientRect().top;
 
-  const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
 
   const limit = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
 
   const maxScroll = limit - window.innerHeight;
 
-  const int = setInterval(function (destElement, destScroll, currentScroll) {
-    console.log(destElement);
-    console.log(destScroll);
-    console.log(currentScroll);
+  const int = setInterval(() => {
 
-    window.scrollTo(0, currentScroll);
-    currentScroll+=10;
+    window.scrollTo(0,currentScroll);
+    currentScroll+=20;
 
-    if(destScroll == currentScroll || currentScroll == maxScroll) {
+    if(currentScroll >= destScroll || currentScroll == maxScroll) {
       clearInterval(int);
       destElement.focus();
     }
 
-  }, 1000/60)
+  },1000/60);
 }
 
-export {smoothScroll};
+export {smoothScroll}
