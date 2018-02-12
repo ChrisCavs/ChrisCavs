aa295269e301645e59158e51f476a2ce815b905e
Chris <chris211321@gmail.com>
2018-02-12 13:57:39 -0500

    style components and slight change to js

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,8 @@ import {smoothScroll} from './smoothScroll';
 
 const main = () => {
 
-  const fadeLeft = Array.from(document.querySelectorAll('.fadeLeft'));
-  const fadeRight = Array.from(document.querySelectorAll('.fadeRight'));
+  const fadeLeft = Array.from(document.querySelectorAll('.fade-left'));
+  const fadeRight = Array.from(document.querySelectorAll('.fade-right'));
 
   //check if content is in view, then fade it in
   const checkContent = () => {
@@ -34,12 +34,9 @@ const main = () => {
   window.addEventListener('scroll', debounce(checkContent, 10));
 
   //listeners for smooth anchor scrolling
-  document.querySelector('.nav-projects').addEventListener('click', function() {
-    smoothScroll('projects');
-  })
-  document.querySelector('.nav-contact').addEventListener('click', function() {
-    smoothScroll('contact');
-  })
+  document.querySelector('.nav-projects').addEventListener('click', () => smoothScroll('projects'));
+  document.querySelector('.nav-contact').addEventListener('click', () => smoothScroll('contact'));
 }
+
 
 document.addEventListener('DOMContentLoaded', main)
