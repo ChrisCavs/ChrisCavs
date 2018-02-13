dadfa8f08913e1f394805d8329700578684c1401
Chris <chris211321@gmail.com>
2018-02-13 12:05:53 -0500

    rework of smoothScroll

--- src/js/smoothScroll.js
+++ src/js/smoothScroll.js
@@ -1,34 +1,47 @@
 const smoothScroll = (destination) => {
 
   //Apply fade-ins before scrolling past
-  const fadeLeft = Array.from(document.querySelectorAll('.fade-left'));
+  const fadeLeft = Array.from(document.querySelectorAll('.fade-left'))
+
   fadeLeft.forEach(item => {
-    item.classList.add('active');
-  });
+    item.classList.add('active')
+  })
 
-  //find the destination element, and its scroll position
-  const destElement = document.querySelector(`.${destination}`);
-  const destScroll = destElement.getBoundingClientRect().top;
+  //find the destination element and its scroll position
+  const destElement = document.querySelector(`.${destination}`)
+  const destScroll = destElement.getBoundingClientRect().top
 
-  let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  //define starting position, and set ticker
+  let start
+  let currentScroll = start = document.documentElement.scrollTop || document.body.scrollTop
+  let ticker = 0
 
-  requestAnimationFrame(loop);
-
+  //define the loop
   function loop () {
 
-    window.scrollTo(0,currentScroll);
-    currentScroll+=20;
+    window.scrollTo(0,currentScroll)
+
+    //speed up till half-way, then slow down (linear easing)
+    currentScroll < (destScroll - start)/2
+      ? ticker += 2
+      : ticker -= 2
+
+    currentScroll+= ticker;
 
     currentScroll < destScroll
       ? requestAnimationFrame(loop)
       : end()
   }
 
+  //define loop end
   function end () {
 
-    window.scrollTo(0,destScroll);
-    destElement.focus();
+    window.scrollTo(0,destScroll)
+    destElement.focus()
   }
+
+  //start the loop
+  requestAnimationFrame(loop)
 }
 
 export {smoothScroll}
