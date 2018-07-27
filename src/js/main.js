47c4340d595b3c6c29b8401cba1356482c67d331
Chris <chris211321@gmail.com>
2018-07-27 19:06:53 -0400

    clarify html, add styles for new format, add jump

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
-import {smoothScroll} from './smoothScroll'
+import jump from 'jump.js'
 
 const main = () => {
+  const contactButton = document.querySelector('.nav-contact')
 
   const fadeLeft = document.querySelector('.fade-left')
   const fadeRight = document.querySelector('.fade-right')
@@ -11,7 +12,13 @@ const main = () => {
   //fade profile image on page-load
   setTimeout(() => {
     fadeLeft.classList.add('active')
-  }, 500)
+  }, 300)
+
+  //add jump to 'contact' button
+  contactButton.addEventListener('click', () => {
+    console.log('fire')
+    jump('.contact', { duration: 1500 })
+  })
 }
 
 
